7a1ae337fa40836a5b8f2f69251e3b93723c9996
Bruce Li <ryu9827@gmail.com>
2017-10-18 18:08:08 +1300

    add contact
    add contact

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -21,10 +21,20 @@ export default Ember.Controller.extend({
 
     actions: {
         sendMsg(){
+            const email = this.get('emailAddress');
+            const message = this.get('message');
+
+            const newContact = this.store.createRecord('contact', {
+                email: email,
+                message:message
+            });
+
+            newContact.save().then((response) => {
             alert(`Sending message to the following email is in progress: ${this.get('emailAddress')}. Message is: ${this.get('message')}`);
-            this.set('responseMessage', `We have saved your email: ${this.get('emailAddress')}. We got your message and we’ll get in touch soon.`);
+            this.set('responseMessage', `You have sent message '${this.get('message')}' to email: ${this.get('emailAddress')}. `);
             this.set('emailAddress', '');
             this.set('message','');
+            });
         }
     }
 });
--- app/routes/admin/contacts.js
+++ app/routes/admin/contacts.js
@@ -1,9 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
-    model() {
-        return this.store.findAll('invitation');
+    model(){
+        this.store.findAll('contact');
     }
-
 });
